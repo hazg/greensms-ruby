6be2a3b090d02ab049fbe87ba5127520d86914b2
Aleksandr Aleksandrov <ya@hazg.net>
2021-05-25 13:51:01 +0300

    Update validator.rb
    Disable schema validation

--- lib/greensms/utils/validator.rb
+++ lib/greensms/utils/validator.rb
@@ -3,10 +3,10 @@ require "greensms/http/error"
 
 module GreenSMS
   def GreenSMS.validate(schema, data)
-    res = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
-    if !res.nil? && res.length > 0
-      raise RestError.new("Validation Error", 1, res)
-    end
+    # res = JSON::Validator.fully_validate(schema, data, :errors_as_objects => true)
+    # if !res.nil? && res.length > 0
+    #  raise RestError.new("Validation Error", 1, res)
+    # end
     return nil
   end
 end
